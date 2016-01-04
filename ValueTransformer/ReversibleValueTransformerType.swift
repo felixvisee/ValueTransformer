b8fbdc1ca58ae4181bb65219506c5d6af73ac3be
Sendy Halim <sendyhalim93@gmail.com>
2016-01-05 01:42:03 +0700

    Rename typealias ErrorType to VTErrorType

--- ValueTransformer/ReversibleValueTransformerType.swift
+++ ValueTransformer/ReversibleValueTransformerType.swift
@@ -3,17 +3,17 @@
 import Result
 
 public protocol ReversibleValueTransformerType: ValueTransformerType {
-    func reverseTransform(transformedValue: TransformedValueType) -> Result<ValueType, ErrorType>
+    func reverseTransform(transformedValue: TransformedValueType) -> Result<ValueType, VTErrorType>
 }
 
 // MARK: - Basics
 
 @available(*, introduced=1.0, deprecated=2.1, message="Use valueTransformer.reverseTransform(transformedValue).")
-public func reverseTransform<V: ReversibleValueTransformerType>(reversibleValueTransformer: V, transformedValue: V.TransformedValueType) -> Result<V.ValueType, V.ErrorType> {
+public func reverseTransform<V: ReversibleValueTransformerType>(reversibleValueTransformer: V, transformedValue: V.TransformedValueType) -> Result<V.ValueType, V.VTErrorType> {
     return reversibleValueTransformer.reverseTransform(transformedValue)
 }
 
-public func reverseTransform<V: ReversibleValueTransformerType>(reversibleValueTransformer: V) -> V.TransformedValueType -> Result<V.ValueType, V.ErrorType> {
+public func reverseTransform<V: ReversibleValueTransformerType>(reversibleValueTransformer: V) -> V.TransformedValueType -> Result<V.ValueType, V.VTErrorType> {
     return { transformedValue in
         return reversibleValueTransformer.reverseTransform(transformedValue)
     }
--- ValueTransformer/ValueTransformerType.swift
+++ ValueTransformer/ValueTransformerType.swift
@@ -5,19 +5,19 @@ import Result
 public protocol ValueTransformerType {
     typealias ValueType
     typealias TransformedValueType
-    typealias ErrorType
+    typealias VTErrorType: ErrorType
 
-    func transform(value: ValueType) -> Result<TransformedValueType, ErrorType>
+    func transform(value: ValueType) -> Result<TransformedValueType, VTErrorType>
 }
 
 // MARK: - Basics
 
 @available(*, introduced=1.0, deprecated=2.1, message="Use valueTransformer.transform(value).")
-public func transform<V: ValueTransformerType>(valueTransformer: V, value: V.ValueType) -> Result<V.TransformedValueType, V.ErrorType> {
+public func transform<V: ValueTransformerType>(valueTransformer: V, value: V.ValueType) -> Result<V.TransformedValueType, V.VTErrorType> {
     return valueTransformer.transform(value)
 }
 
-public func transform<V: ValueTransformerType>(valueTransformer: V) -> V.ValueType -> Result<V.TransformedValueType, V.ErrorType> {
+public func transform<V: ValueTransformerType>(valueTransformer: V) -> V.ValueType -> Result<V.TransformedValueType, V.VTErrorType> {
     return { value in
         valueTransformer.transform(value)
     }
